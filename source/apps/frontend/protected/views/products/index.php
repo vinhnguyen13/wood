c43c6d8ded5c1df261b5d668cc4c7511bb23be16
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-05-03 18:30:02 +0700

    product

--- source/apps/frontend/protected/views/products/index.php
+++ source/apps/frontend/protected/views/products/index.php
@@ -3,6 +3,16 @@
 
 $this->pageTitle = Yii::app()->name;
 ?>
+<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl; ?>/resources/js/jquery.mousewheel-3.0.6.pack.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl; ?>/resources/js/jquery.fancybox.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/resources/css/jquery.fancybox.css" media="screen" />
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.fancybox').fancybox();
+
+    });
+</script>
+
 <div class="content-top">
     <div class="wrap">
         <div class="content-full products">
